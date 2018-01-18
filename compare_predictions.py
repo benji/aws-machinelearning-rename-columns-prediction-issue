7822c79e80cdf69ae3103b0623ba747351c92351
Benjamin Jaton <benjamin.jaton@gmail.com>
2018-01-18 00:23:34 -0800

    Predictions quantitative diff

--- compare_predictions.py
+++ compare_predictions.py
@@ -2,14 +2,15 @@ import pandas as pd
 import math, json, sys
 import numpy as np
 
-df1 = pd.read_csv('data/1stFlrSF/sample_predictions.csv')
-df2 = pd.read_csv('data/FirstFlrSF/sample_predictions.csv')
+df1 = pd.read_csv('data/FirstFlrSF/sample_predictions.csv')
+df2 = pd.read_csv('data/1stFlrSF/sample_predictions.csv')
 
 df =  pd.DataFrame()
 df['tag'] = df1['tag']
 df['score1'] = df1['score']
 df['score2'] = df2['score']
 
-df['diff'] = (df['score2'] - df['score1']) / df['score1'] * 100
+df['diff'] = (df['score2'] - df['score1']).abs() / df['score1'] * 100
 
 print df.sort_values('diff', ascending=False).head()
+print 'Average diff', np.mean(df['diff'])
